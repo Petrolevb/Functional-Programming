fc7c3ac6c7d41139e271f856b8953309354b7a9d
Petrolevb-XIrisProg <Vincebellec@gmail.com>
2012-10-31 11:50:05 +0100

    Add gameOver and Empty

--- ass2/BlackJack.hs
+++ ass2/BlackJack.hs
@@ -1,14 +1,28 @@
 module BlackJack where
 
-import Cards
 import Wrapper
+import Cards
 
 
 -- Define a function returning an empty hand
 
-empty :: Hand
+empty :: Hand = []
 
 
 -- From a given hand, calculate the value
+valueRank :: Rank -> Integer
+valueRank 
+
+valueCard :: Card -> Integer
 
 value :: Hand -> Integer
+
+
+-- From a given hand, determinate if loose
+gameOver :: Hand -> Bool
+gameOver n | (value n > 21) = True
+gameOver n = False
+
+
+-- Determinate between two hands who wins
+winner :: Hand -> Hand -> Player
