e6141a7243cbcd9fa0b000b7efc1acd02bec63c2
Petrolevb-XIrisProg <Vincebellec@gmail.com>
2012-10-31 12:00:31 +0100

    Ajout Values

--- ass2/BlackJack.hs
+++ ass2/BlackJack.hs
@@ -6,14 +6,18 @@ import Cards
 
 -- Define a function returning an empty hand
 
-empty :: Hand = []
+empty :: Hand = Empty
 
 
 -- From a given hand, calculate the value
 valueRank :: Rank -> Integer
-valueRank 
+valueRank (Numeric n) = n
+valueRank Jack = 10
+valueRank Queen = 10
+valueRank King = 10
 
 valueCard :: Card -> Integer
+valueCard c = valueRank (rank c)
 
 value :: Hand -> Integer
 
