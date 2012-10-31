7536dff2346e35dcdd9c4b9fcb9f6fce6c4be2aa
Petrolevb-XIrisProg <Vincebellec@gmail.com>
2012-10-31 12:21:38 +0100

    ajout operator <+

--- ass2/BlackJack.hs
+++ ass2/BlackJack.hs
@@ -12,28 +12,44 @@ empty :: Hand = Empty
 -- From a given hand, calculate the value
 valueRank :: Rank -> Integer
 valueRank (Numeric n) = n
-valueRank Jack = 10
-valueRank Queen = 10
-valueRank King = 10
+valueRank Jack 	      = 10
+valueRank Queen       = 10
+valueRank King        = 10
 
 valueCard :: Card -> Integer
 valueCard c = valueRank (rank c)
 
 
 numberOfAces :: Hand->Integer
-numberOfAces Empty = 0
+numberOfAces Empty 	  = 0
 numberOfAces (Add Aces h) = 1 + numberOfAces h
-numberOfAces (Add _ h ) = numberOfAces h
+numberOfAces (Add _ h )   = numberOfAces h
 
 value :: Hand -> Integer
-value Empty = 0
-value (Add c h)) = valueCard c + value h
+value Empty     = 0
+value (Add c h) = valueCard c + value h
 
 -- From a given hand, determinate if loose
 gameOver :: Hand -> Bool
-gameOver n | (value n > 21) = True
-gameOver n = False
+gameOver n | (value n > 21) 	= True
+gameOver n 			= False
 
 
--- Determinate between two hands who wins
+-- Determinate between two hands who wins Guest in first, Bank in second
 winner :: Hand -> Hand -> Player
+winner Empty Empty 	           = Bank
+winner _ Empty 			   = Guest
+winner h hb | (value h > value hb) = Guest
+winner h hb 			   = Bank
+
+
+-- Add an Hand on the other
+<+ :: Hand -> Hand -> Hand
+Empty <+ h      = h
+h <+ Empty      = h
+(Add c h) <+ hb =  (Add c (h <+ hb))
+
+
+
+
+
