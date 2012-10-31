9bed3b674381fc46287b5d5528f3531706f76b2f
Petrolevb-XIrisProg <Vincebellec@gmail.com>
2012-10-31 11:21:17 +0100

    Functions to do

---
+++ ass2/BlackJack.hs
@@ -0,0 +1,14 @@
+module BlackJack where
+
+import Cards
+import Wrapper
+
+
+-- Define a function returning an empty hand
+
+empty :: Hand
+
+
+-- From a given hand, calculate the value
+
+value :: Hand -> Integer
