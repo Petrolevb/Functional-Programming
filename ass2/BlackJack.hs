6f79a359f1c478fe149a678969f545ebaa4d5317
Wambou <lewambou@gmail.com>
2012-11-04 13:10:14 +0100

    Update ass2/BlackJack.hs

--- ass2/BlackJack.hs
+++ ass2/BlackJack.hs
@@ -4,9 +4,9 @@ import Wrapper
 import Cards
 
 {-
-   Execution of size hand2
+   Execution of size hand
 
-   size hand2
+   size hand
    = size (Add (Card (Numeric 2) Hearts)
                (Add (Card Jack Spades) Empty))
    = 1 + size (Add (Card Jack Spades) Empty)
@@ -25,7 +25,7 @@ empty = Empty
 -- Return the rank of a card
 valueRank :: Rank -> Integer
 valueRank (Numeric n) = n
-valueRank Jack 	      = 10
+valueRank Jack          = 10
 valueRank Queen       = 10
 valueRank King        = 10
 valueRank Ace         = 11
@@ -65,6 +65,7 @@ winner h hb                                           = Bank
 
 
 -- Add an Hand on the other
+infixr 5 <+ -- same priority as :
 (<+) :: Hand -> Hand -> Hand
 Empty <+ h      = h
 h <+ Empty      = h
@@ -82,5 +83,5 @@ prop_onTopOf assoc p1 p2 p3 = p1 <+ (p2 <+ p3 ) == (p1 <+ p2 ) <+ p3
 hand1 = Add (Card Ace Hearts) (Add (Card Ace Spades) Empty) -- 2
 hand2 = Add (Card Ace Hearts) (Add (Card King Spades) Empty) -- 21
 hand3 = Add (Card Ace Hearts) (Add (Card King Spades) (Add (Card (Numeric 8) Hearts) Empty)) -- 19
-hand4 = Add (Card Ace Hearts) (Add (Card Ace Spades) (Add (Card (Numeric 8) Hearts) Empty)) -- 8
+hand4 = Add (Card Ace Hearts) (Add (Card Ace Spades) (Add (Card (Numeric 8) Hearts) Empty)) -- 10
 hand5 = Add (Card (Numeric 3) Hearts) (Add (Card King Spades) (Add (Card Queen Hearts) Empty)) -- 22
