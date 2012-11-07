eb26943e00233c022b63dd5ec184e1feb421aed3
Wambou <lewambou@gmail.com>
2012-11-08 00:52:27 +0100

    correction de erreur. Ajout des mains de test et brouillon de la suite

--- ass2/BlackJack.hs
+++ ass2/BlackJack.hs
@@ -39,6 +39,11 @@ numberOfAces Empty 	  = 0
 numberOfAces (Add (Card Ace _) h) = 1 + numberOfAces h
 numberOfAces (Add _ h )   = numberOfAces h
 
+-- Return the value of a hand when there is more than one ace
+valueMultiAces :: Hand -> Integer
+valueMultiAces Empty = 0
+valueMultiAces (Add c h) = valueMultiAces h + valueCard c 
+
 -- Return the value of a hand
 value :: Hand -> Integer
 value Empty     = 0
@@ -50,11 +55,6 @@ value (Add c h)	| numberOfAces (Add c h) > 1  = valueMultiAces (Add c h)
 			else valueCard c + value h + 10
 
 
--- 
-valueMultiAces :: Hand -> Integer
-valueMultiAces Empty = 0
-valueMultiAces (Add c h) = valueMultiAces h + valueCard c 
-
 -- From a given hand, determinate if loose
 gameOver :: Hand -> Bool
 gameOver n = value n > 21
@@ -62,10 +62,9 @@ gameOver n = value n > 21
 
 -- Determinate between two hands who wins Guest in first, Bank in second
 winner :: Hand -> Hand -> Player
-winner Empty Empty                                    = Bank
-winner _ Empty                                        = Guest
-winner h hb | (value h > value hb) && not(gameOver h) = Guest
-winner h hb                                           = Bank
+winner h hb | gameOver h  = Bank
+            | gameOver hb = Guest
+            | otherwise   = if value h > value hb then Guest else Bank
 
 
 -- Add an Hand on the other
@@ -83,3 +82,30 @@ prop_onTopOf_assoc p1 p2 p3 = p1 <+ (p2 <+ p3 ) == (p1 <+ p2 ) <+ p3
 prop_onTopOf :: Hand -> Hand -> Bool
 prop_onTopOf p1 p2 = size(p1 <+ p2) == size p1 + size p2
 
+
+-- Creating a full Deck
+{-
+createSuit :: Suit -> Hand -> Hand
+createSuit s Empty                 = createSuit s (Add (Card Ace s) Empty)
+createSuit s h  | size h + 1 <= 10 = createSuit s (Add (Card (Numeric (size h + 1)) s) h)
+                | size h + 1 == 11 = createSuit s (Add (Card Jack s) h)
+                | size h + 1 == 12 = createSuit s (Add (Card Queen s) h)
+                | size h + 1 == 13 = createSuit s (Add (Card King s) h)
+
+
+h = createSuit Hearts Empty
+h2 = createSuit Hearts (Add (Card Ace Hearts) Empty)
+
+fullDeck :: Hand
+fullDeck = undefined
+-}
+
+
+-- Hand test
+hand1 = Add (Card Ace Hearts) (Add (Card Ace Spades) Empty) -- 2
+hand2 = Add (Card Ace Hearts) (Add (Card King Spades) Empty) -- 21
+hand3 = Add (Card Ace Hearts) (Add (Card King Spades) (Add (Card (Numeric 8) Hearts) Empty)) -- 19
+hand4 = Add (Card Ace Hearts) (Add (Card Ace Spades) (Add (Card (Numeric 8) Hearts) Empty)) -- 10
+hand5 = Add (Card (Numeric 3) Hearts) (Add (Card King Spades) (Add (Card Queen Hearts) Empty)) -- 23
+hand6 = Add (Card Ace Spades) (Add (Card Ace Diamonds) (Add (Card King Spades) (Add (Card Ace Hearts) Empty))) -- 13
+hand7 = Add (Card (Numeric 8) Hearts) (Add (Card Ace Diamonds)  (Add (Card (Numeric 3) Hearts) Empty)) -- 12
