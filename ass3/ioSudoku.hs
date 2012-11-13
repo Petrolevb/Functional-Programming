f8c88f7d776a8170908a23992e22d05537653ca0
Petrolevb-XIrisProg <Vincebellec@gmail.com>
2012-11-13 14:01:18 +0100

    Affichage des sudoku, vCrade

--- ass3/ioSudoku.hs
+++ ass3/ioSudoku.hs
@@ -1,5 +1,21 @@
 
 import BaseSudoku
 
-printSudoku :: Sudoku -> IO String
-printSudoku = undefined
+
+-- Function to print the Sudoku
+printSudoku :: Sudoku -> IO ()
+printSudoku s = do
+  printLine $ map (\x -> map (\y -> printCase y) x) (rows s)
+
+-- Cette fonction affiche toutes les lignes d'un tableau de string
+printLine :: [String] -> IO ()
+printLine [] = return ()
+printLine (a:as) = do 
+  putStrLn a
+  printLine as
+
+
+-- This function will convert the Maybe Int into a single char
+printCase ::(Maybe Int) -> Char
+printCase Nothing = '.'
+printCase (Just n) = show n !! 0
