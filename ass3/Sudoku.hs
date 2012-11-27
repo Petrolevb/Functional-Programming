aaf7f426bec4ae0ad97ad5d0db6faba6cc993b69
Wambou <lewambou@gmail.com>
2012-11-27 22:19:18 +0100

    probleme de merge

--- ass3/Sudoku.hs
+++ ass3/Sudoku.hs
@@ -309,3 +309,18 @@ easy =
        , [Just 8 , Nothing, Nothing,   Just 2 , Nothing, Just 3 ,   Nothing, Nothing, Just 9 ]
        , [Nothing, Nothing, Just 5 ,   Nothing, Just 1 , Nothing,   Just 3 , Nothing, Nothing]
        ]
+
+fini =
+    Sudoku
+      [ [Just 3 , Just 6 , Nothing,   Nothing, Just 7 , Just 1 ,   Just 2 , Nothing, Nothing]
+      , [Nothing, Just 5 , Just 2 ,   Just 9 , Nothing, Just 6 ,   Just 1 , Just 8 , Just 4 ]
+      , [Just 8 , Just 1 , Just 9 ,   Just 2 , Just 5 , Just 4 ,   Just 7 , Just 3 , Just 6 ]
+
+      , [Just 5 , Just 9 , Just 6 ,   Just 7 , Just 1 , Just 3 ,   Just 4 , Just 2 , Just 8 ]
+      , [Just 4 , Just 3 , Just 1 ,   Just 5 , Just 8 , Just 2 ,   Just 6 , Just 7 , Just 9 ]
+      , [Just 2 , Just 7 , Just 8 ,   Just 4 , Just 6 , Just 9 ,   Just 3 , Just 5 , Just 1 ]
+
+      , [Just 6 , Just 4 , Just 5 ,   Just 3 , Just 2 , Just 8 ,   Just 9 , Just 1 , Just 7 ]
+      , [Just 9 , Just 8 , Just 3 ,   Just 1 , Just 4 , Just 7 ,   Just 5 , Just 6 , Just 2 ]
+      , [Just 1 , Just 2 , Just 7 ,   Just 6 , Just 9 , Just 5 ,   Just 8 , Just 4 , Just 3 ]
+      ]
