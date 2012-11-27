7d6871182586bb196da7dd4416c2e80b39287f97
Wambou <lewambou@gmail.com>
2012-11-27 14:52:49 +0100

    Fonction solve boucle en permanance, ajout du sudoku easy

--- ass3/Sudoku.hs
+++ ass3/Sudoku.hs
@@ -259,13 +259,13 @@ solve' sud = testPosition blankPos sud
 
 testPosition [] sud = if isOkay sud then Just sud else Nothing
 testPosition (p:ps) sud | isNothing (testValue p (values p) sud) = testPosition ps sud
-                        | otherwise = testValue p (values p) sud
+                        | otherwise = solve ( fromJust (testValue p (values p) sud))
   where values = candidates sud
 
 testValue :: Pos -> [Int] -> Sudoku -> Maybe Sudoku
 testValue pos [] sud = if isOkay sud then Just sud else Nothing
 testValue pos (v:vs) sud | isNothing (solve tmpSud) = testValue pos vs sud
-                         | otherwise = Just tmpSud
+                         | otherwise = solve tmpSud
   where tmpSud = update sud pos (Just v)
 
  {-
@@ -294,3 +294,18 @@ ex =
       , [Nothing, Just 8 , Just 3 ,   Nothing, Nothing, Nothing,   Nothing, Just 6 , Nothing]
       , [Nothing, Nothing, Just 7 ,   Just 6 , Just 9 , Nothing,   Nothing, Just 4 , Just 3 ]
       ]
+
+easy =
+     Sudoku
+       [ [Nothing, Nothing, Just 3 ,   Nothing, Just 2 , Nothing,   Just 6 , Nothing, Nothing]
+       , [Just 9 , Nothing, Nothing,   Just 3 , Nothing, Just 5 ,   Nothing, Nothing, Just 1 ]
+       , [Nothing, Nothing, Just 1 ,   Just 8 , Nothing, Just 6 ,   Just 4 , Nothing, Nothing]
+
+       , [Nothing, Nothing, Just 8 ,   Just 1 , Nothing, Just 2 ,   Just 9 , Nothing, Nothing]
+       , [Just 7 , Nothing, Nothing,   Nothing, Nothing, Nothing,   Nothing, Nothing, Just 8 ]
+       , [Nothing, Nothing, Just 6 ,   Just 7 , Nothing, Just 8 ,   Just 2 , Nothing, Nothing]
+
+       , [Nothing, Nothing, Just 2 ,   Just 6 , Nothing, Just 9 ,   Just 5 , Nothing, Nothing]
+       , [Just 8 , Nothing, Nothing,   Just 2 , Nothing, Just 3 ,   Nothing, Nothing, Just 9 ]
+       , [Nothing, Nothing, Just 5 ,   Nothing, Just 1 , Nothing,   Just 3 , Nothing, Nothing]
+       ]
