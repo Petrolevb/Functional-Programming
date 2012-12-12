1e1965d5967abb6077f4314b2b157a3dd271e0e1
Wambou <lewambou@gmail.com>
2012-12-12 01:36:36 +0100

    Ajout de commentaires

--- projet/Board.hs
+++ projet/Board.hs
@@ -9,13 +9,15 @@ printBoard b = do putStr "\ESC[2J"
 
 printB :: [(Case, a)] -> IO()
 printB [] = putStrLn ""
-printB ((c, m):bs) | (21 < length list && length list <= 24)
+printB ((c, m):bs) | -- Create the first and last line for the board
+                     (21 < length list && length list <= 24)
                      || length list <= 3 = do
                                               putStrLn $
                                                createLine ((c,m):(take 2 bs))
                                                " --------------------- " ""
                                               printB $ drop 2 bs
-                   | (18 < length list && length list <= 21)
+                   | -- Create first and last line for the middle square
+                     (18 < length list && length list <= 21)
                      || (3 < length list && length list <= 6)
                      = do
                           putStrLn $ "|     "
@@ -23,7 +25,8 @@ printB ((c, m):bs) | (21 < length list && length list <= 24)
                                    " --------------- "
                                    "     |"
                           printB $ drop 2 bs
-                   | (15 < length list && length list <= 18)
+                   | -- Create the first and last line for the smallest square
+                     (15 < length list && length list <= 18)
                      || (6 < length list && length list <= 9)
                      = do
                           putStrLn $ "|     |     "
@@ -31,7 +34,8 @@ printB ((c, m):bs) | (21 < length list && length list <= 24)
                                       " --------- "
                                       "     |     |"
                           printB $ drop 2 bs
-                   | otherwise = do
+                   | -- Create the line in the middle of the board
+                     otherwise = do
                                    putStr $ createLine ((c,m):(take 2 bs))
                                              " --- "
                                              "                       "
@@ -40,13 +44,13 @@ printB ((c, m):bs) | (21 < length list && length list <= 24)
                                                ""
                                    printB $ drop 5 bs
   where list = (c, m):bs
+        -- Create lines for output out of an array and some formated strings
         createLine (s:[]) dash after = getCase (fst s) ++ after
         createLine (s:ss) dash after = getCase (fst s) ++ dash ++ createLine ss dash after
+
 
 getCase :: Case -> [Char]
 getCase c = case c of
                       Case NineMen.Red   -> "H"
                       Case NineMen.Black -> "C"
                       _                  -> "°"
-
-gameLoop = undefined
