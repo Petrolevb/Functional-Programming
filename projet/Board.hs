4410da6a50eeb9965f7763c791959b5e703d88c7
Wambou <lewambou@gmail.com>
2012-12-12 01:31:47 +0100

    Affichage du plateau de jeu

--- projet/Board.hs
+++ projet/Board.hs
@@ -7,27 +7,46 @@ printBoard :: Board -> IO()
 printBoard b = do putStr "\ESC[2J"
                   printB $ line b
 
+printB :: [(Case, a)] -> IO()
 printB [] = putStrLn ""
-printB ((c, a):bs) | length list == 24
-                     || length list == 15
-                     || not (isMod3 list) = do 
-                                              printCase c
-                                              printB    bs
+printB ((c, m):bs) | (21 < length list && length list <= 24)
+                     || length list <= 3 = do
+                                              putStrLn $
+                                               createLine ((c,m):(take 2 bs))
+                                               " --------------------- " ""
+                                              printB $ drop 2 bs
+                   | (18 < length list && length list <= 21)
+                     || (3 < length list && length list <= 6)
+                     = do
+                          putStrLn $ "|     "
+                                   ++ createLine ((c,m):(take 2 bs))
+                                   " --------------- "
+                                   "     |"
+                          printB $ drop 2 bs
+                   | (15 < length list && length list <= 18)
+                     || (6 < length list && length list <= 9)
+                     = do
+                          putStrLn $ "|     |     "
+                                   ++ createLine ((c,m):(take 2 bs))
+                                      " --------- "
+                                      "     |     |"
+                          printB $ drop 2 bs
                    | otherwise = do
-                                    putStrLn  ""
-                                    printCase c
-                                    printB    bs
-  where list = (c, a):bs
-        isMod3 b = length b `mod` 3 == 0
-        printCase c = putStr $ case c of
-                                    Case NineMen.Red   -> " R "
-                                    Case NineMen.Black -> " B "
-                                    _          -> "   "
+                                   putStr $ createLine ((c,m):(take 2 bs))
+                                             " --- "
+                                             "                       "
+                                   putStrLn $ createLine (take 3 (drop 2 bs))
+                                               " --- "
+                                               ""
+                                   printB $ drop 5 bs
+  where list = (c, m):bs
+        createLine (s:[]) dash after = getCase (fst s) ++ after
+        createLine (s:ss) dash after = getCase (fst s) ++ dash ++ createLine ss dash after
 
-
+getCase :: Case -> [Char]
 getCase c = case c of
-                      Case NineMen.Red   -> " R "
-                      Case NineMen.Black -> " B "
-                      _          -> "   " 
+                      Case NineMen.Red   -> "H"
+                      Case NineMen.Black -> "C"
+                      _                  -> "°"
 
 gameLoop = undefined
