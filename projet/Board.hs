add08babeeded22e7d7b957963f943ebcb8cd549
Petrolevb-XIrisProg <Vincebellec@gmail.com>
2012-12-12 01:23:46 +0100

    Premiere instanciation de la boucle de jeu

--- projet/Board.hs
+++ projet/Board.hs
@@ -2,7 +2,8 @@ module Board where
 
 import System.Console.ANSI
 import NineMen
-
+import Data.Maybe (isNothing, fromJust)
+import GHC.IO
 
 printBoard :: Board -> IO()
 printBoard b = do putStr "\ESC[2J"
@@ -33,37 +34,45 @@ getCase c = case c of
 
 
 gameLoop :: Board -> IO()
-gameLoop b | win       b = showWin
-           | lose      b = showLose
-           | otherwise   = 
+gameLoop b | win       = do showWin
+           | lose      = do showLose
+           | otherwise = do 
                     printBoard b
                     putStrLn "Enter two numbers : which token move, enter key then the position"
-                    let (newBoard, alignements) = createAlignement b moveFrom moveTo
                     if isNothing newBoard 
                         then gameLoop b 
-                    else 
-                        if alignements 
-                            then gameLoop (removeChoosenToken newBoard) 
-                        else gameLoop newBoard
+                        else if alignements 
+                                 then do
+                                    putStrLn "Enter a token to remove : " 
+                                    gameLoop (removeChoosenToken $ fromJust newBoard) 
+                                 else gameLoop $ fromJust newBoard
     where 
-        win      = winner b == Red
-        lose     = winner b == Black
-        showWin  = "Win Red"
-        showLose = "Win Black"
-        moveFrom = askPosition
-        moveTo   = askPosition
+        win                     = winner b == Just NineMen.Red
+        lose                    = winner b == Just NineMen.Black
+        showWin                 = putStrLn "Win Red"
+        showLose                = putStrLn "Win Black"
+        moveFrom                = askPosition
+        moveTo                  = askPosition
+        (newBoard, alignements) = createAlignement b moveFrom moveTo
 
 
 removeChoosenToken :: Board -> Board
-removeChoosenToken b = putStrLn "Which token do you want to remove ?"
-                       let newBoard = removeToken b askPosition
-                       if isNothing newBoard then removeChoosenToken b else
-                       fromJust newBoard
+removeChoosenToken b =  
+    let newBoard = removeToken b askPosition in
+    if isNothing newBoard 
+        then removeChoosenToken b 
+        else fromJust newBoard
 
 askPosition :: Int 
-askPosition = do 
-    position <- read :: Int
+askPosition = let p = fromIOPosition (readLn :: IO Int) in
+              if p == -1 
+                then askPosition 
+                else p
+
 
 fromIOPosition :: IO Int -> Int
-fromIOPosition IO i | i >= 0 && i < 24 = i
-                    | otherwise        = -1
+{-# NOINLINE fromIOPosition #-} 
+fromIOPosition i = let pos = unsafePerformIO i in
+                if pos >= 0 && pos < 24 
+                    then pos 
+                    else -1
