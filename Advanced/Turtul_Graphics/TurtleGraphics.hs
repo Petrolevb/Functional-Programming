e914d091fac6bd68e0b16f752d083ca583cf34ef
Wambou <lewambou@gmail.com>
2013-01-22 11:59:32 +0100

    Retrait de certaines lignes de code inutiles

--- Advanced/Turtul_Graphics/TurtleGraphics.hs
+++ Advanced/Turtul_Graphics/TurtleGraphics.hs
@@ -4,7 +4,6 @@ import Graphics.UI.GLUT
 main :: IO ()
 main = do
   (progname, _) <- getArgsAndInitialize
-  initialDisplayMode $= [ SingleBuffered, RGBMode, WithDepthBuffer ]
   initialWindowSize $= Size 500 500
   createWindow "Turtle Graphics"
   displayCallback $= display
@@ -20,19 +19,16 @@ myCube =
   do
     color $ (Color3 (1.0::GLfloat) 0 0)
     vertex $ (Vertex3 (0  ::GLfloat) 0   0)
-    vertex $ (Vertex3 (0.2::GLfloat) 0.1   0)
+    vertex $ (Vertex3 (0.2::GLfloat) 0.1 0)
     vertex $ (Vertex3 (0.2::GLfloat) 0.4 0)
     vertex $ (Vertex3 (0  ::GLfloat) 0.3 0)
     color $ (Color3 (0::GLfloat) 1.0 0)
     vertex $ (Vertex3 (0   ::GLfloat) 0   0)
-    vertex $ (Vertex3 (-0.2::GLfloat) 0.1   0)
+    vertex $ (Vertex3 (-0.2::GLfloat) 0.1 0)
     vertex $ (Vertex3 (-0.2::GLfloat) 0.4 0)
     vertex $ (Vertex3 (0   ::GLfloat) 0.3 0)
     color $ (Color3 (0::GLfloat) 0 1.0)
-    vertex $ (Vertex3 (0   ::GLfloat) 0.3   0)
-    vertex $ (Vertex3 (-0.2::GLfloat) 0.4   0)
+    vertex $ (Vertex3 (0   ::GLfloat) 0.3 0)
+    vertex $ (Vertex3 (-0.2::GLfloat) 0.4 0)
     vertex $ (Vertex3 (0   ::GLfloat) 0.5 0)
-    vertex $ (Vertex3 (0.2 ::GLfloat) 0.4 0)    
-
-myPoints :: [(GLfloat,GLfloat,GLfloat)]
-myPoints = map (\k -> (sin(2*pi*k/12),cos(2*pi*k/12),0.0)) [1..12]
+    vertex $ (Vertex3 (0.2 ::GLfloat) 0.4 0)
