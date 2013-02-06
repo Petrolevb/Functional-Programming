63f3fcf463e9d507259d1e6f362898df84ebb4ad
Wambou <lewambou@gmail.com>
2013-02-06 23:49:14 +0100

    resolution de conflit

--- Advanced/Turtul_Graphics/executable/Exemple.hs
+++ Advanced/Turtul_Graphics/executable/Exemple.hs
@@ -3,10 +3,9 @@ module Main where
 import TurtleGraphics
 import Turtle
 
-main = test
--- main = runGraphical -- $ spiral (startingProgram) 0 91
+main = runGraphical $ spiral startingProgram 0 91
 
-test = runTextual turtle3
+test = runGraphical turtle3
   where turtle = forward startingProgram 0.5
         turtle1 = right turtle 90
         turtle2 = forward turtle1 0.5
@@ -17,7 +16,6 @@ spiral :: Program -> Double -> Double -> Program
 spiral tur size angle | size > 100 = nothing tur
                       | otherwise  = 
             spiral (forward tur size -=> right $ angle) (size + 2) angle
-
 -- ^ Question : Can you use the lifespan command to define the example?
 --       With the lifespan command, we make the turtle move until it dies.
 --       We need to update the size before each forward
