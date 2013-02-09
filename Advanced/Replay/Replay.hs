45816cef843b3d42a73bf1abf1ec7a499723e8c0
Petrolevb-XIrisProg <Vincebellec@gmail.com>
2013-02-09 13:11:22 +0100

    Definition return Replay

--- Advanced/Replay/Replay.hs
+++ Advanced/Replay/Replay.hs
@@ -3,12 +3,20 @@ module Replay where
 import System.Time(getClockTime, ClockTime(TOD))
 
 -- Types
-data Replay q r a
-data Trace r
+newtype Replay q r i = Rep ( q -> r -> i)
+data Trace r = Nil | Item r (Trace r)
+data Item r = Answer r | Result String
+    deriving (Show, Read)
+
+type Question = String
+
 -- Operations
 instance Monad (Replay q r)
-    where return = undefined
-          (>>=)  = undefined
+    where
+--        return :: i -> Replay q r i
+        return i = Rep (\q -> \r -> i)
+--       (>>=) :: Replay q r i1 -> (i1 -> Replay q r i2) -> Replay q r i2
+        (>>=)  = undefined
 instance Show r => Show (Trace r)
 instance Read r => Read (Trace r)
 
@@ -16,17 +24,21 @@ liftIO :: (Show a, Read a) => IO a -> Replay q r a
 liftIO = undefined
 ask  :: q -> Replay q r r
 ask = undefined
+--ask question = do
+--    putStrLn question
+--    answer <- getLine
+--    return (Replay question answer answer)
 
 emptyTrace :: Trace r
-emptyTrace = undefined
+emptyTrace = Nil
 addAnswer  :: Trace r -> r -> Trace r
-addAnswer = undefined
+addAnswer traces a =  Item a traces
 
 run :: Replay q r a -> Trace r -> IO (Either (q, Trace r) a)
 run = undefined
 
 {-
-example :: Replay Int
+example :: TReplay Int
 example = do
     t0 <- liftIO getTime
     liftIO (putStrLn "Hello!")
