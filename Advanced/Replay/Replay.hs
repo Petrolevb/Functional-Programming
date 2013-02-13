eecb8374f08198549944305c5d153eb473cec622
Petrolevb-XIrisProg <Vincebellec@gmail.com>
2013-02-13 15:47:46 +0100

    Avancement

--- Advanced/Replay/Replay.hs
+++ Advanced/Replay/Replay.hs
@@ -5,8 +5,8 @@ import System.Time(getClockTime, ClockTime(TOD))
 -- Types
 --newtype Replay q r i = Rep ( [Lift | Ask] -> r -> IO i)
 data Replay q r i = EmptyReplay 
-                  | LiftIO (IO i) (Trace i) (Replay q r i)
-                  | Ask    q      (Trace i) (Replay q r i)
+                  | LiftIO (IO i) (Trace i)
+                  | Ask    q      (Trace r)
                   | Final i
 data Trace r = Answer r (Trace r) 
              | Result r (Trace r)
@@ -21,17 +21,20 @@ instance Monad (Replay q r)
         return i = Final i
 --       (>>=) :: Replay q r a -> (a -> Replay q r b) -> Replay q r b
 --        EmptyReplay     >>= f = undefined -- cannot be used as a parameter for f
-        (LiftIO io (Result r trace) rep)  >>= f = f r
-        (Ask question Nil rep) >>= f = undefined
-        (Ask question (Answer ans trace) rep) >>= f = f ans
+        (LiftIO io Nil)               >>= f = undefined
+        (LiftIO io (Result r trace))  >>= f = f r
+        (Ask question Nil) >>= f = undefined
+        (Ask question (Answer ans trace)) >>= f = undefined
+
             -- f ans
 --        (Final i)       >>= f = undefined
 
--- liftIO getTime >>= (\t0 -> liftIO (putStrLn "Hello!"))
-
 {-
-    (\x -> liftIO x) (LiftIO getTime (Result "13434" trace) repl
-    (\x -> liftIO x) (Ask "what is your age" (Answer "24" trace) repl
+    liftIO getTime >>=
+    (\t1 -> liftIO (putStrLn ("Total time : " ++ show (t1 - t0) ++ " seconds"))) 
+    
+    (\x -> liftIO x) (LiftIO getTime (Result "13434" trace)
+    (\x -> liftIO x) (Ask "what is your age" (Answer "24" trace)
 
     a = LiftIO (putStr "") (Result "()" Nil) EmptyReplay
     b = LiftIO (putStr "Hello") (Result "()" Nil) EmptyReplay
@@ -44,15 +47,6 @@ instance Monad (Replay q r)
 
 -}
 
---combine :: Replay q r a -> Replay q r b -> Replay q r b
-combine (LiftIO io trace emrep)    rep =  LiftIO io (addTrace trace (getTrace rep)) rep
-    where getTrace (LiftIO _ trace _) = trace
-          getTrace (Ask    _ trace _) = trace
-combine (Ask question trace emrep) rep = Ask question (addTrace trace (getTrace rep)) rep
-    where getTrace (LiftIO _ trace _) = trace
-          getTrace (Ask    _ trace _) = trace
-
---instance Show r => Show (Trace r)
 instance Show r => Show (Trace r) where
     show Nil = "EndTrace"
     show (Answer r trace) = "Answer " ++ show r ++ ", " ++ show trace
@@ -60,11 +54,11 @@ instance Show r => Show (Trace r) where
 instance Read r => Read (Trace r)
 
 liftIO :: (Show a, Read a) => IO a -> Replay q r a
-liftIO io = undefined
---liftIO io = LiftIO io (Result (print.io)) emptyTrace EmptyReplay
+liftIO io = LiftIO io Nil
+--liftIO io = LiftIO io (Result (print.io))  
 
 ask  :: q -> Replay q r r
-ask question = Ask question emptyTrace EmptyReplay
+ask question = Ask question emptyTrace 
 
 emptyTrace :: Trace r
 emptyTrace = Nil
@@ -78,6 +72,11 @@ addTrace (Answer r tr) trace = Answer r (addTrace tr trace)
 
 
 run :: Show a => Replay q r a -> Trace r -> IO (Either (q, Trace r) a)
+--run (Ask q traceExec) Nil = Left (q, traceExec)
+run (Ask q (Answer a traceExec)) (Answer r trace) = undefined
+run (Ask q traceExec) _ = return (Left (q, traceExec))
+run (LiftIO io traceExec) Nil = undefined 
+run (LiftIO io traceExec) traceParam = undefined
 --run (Ask q traceRep next) (Answer a trace) = run next (addAnswer traceRep a)
 --run (Ask q traceRep next) Nil              = return $ Left (q, traceRep)
 --run (LiftIO io trace next) (Result a traceExec) = undefined
@@ -94,20 +93,29 @@ run (Final i) _ = return $ Right i
 --run (Rep f) (Item i trac)  = undefined
 
 
-example :: Replay String Int Int
+running :: Show i => Replay String String i -> IO i
+running prog = play emptyTrace prog
+play t prog = do
+    e <- run prog t
+    case e of
+        Left (q, t') -> do
+            putStr ("Question: " ++ q ++ " ")
+            r <- getLine
+            play (addAnswer t' r) prog
+        Right x -> return x
+
+
+example :: Replay String String Int
 example = do
---    t0 <- liftIO getTime
---    liftIO (putStrLn "Hello!")
-    ask "What's your age ? " >>= (\a -> return a)
---    age <- ask "What is your age ? "
-{-
-    liftIO (putStrLn "Age : " ++ age)
+    t0 <- liftIO getTime
+    liftIO (putStrLn "Hello!")
+    age <- ask "What is your age ? "
+    liftIO (putStrLn ("Age : " ++ age))
     name <- ask "What's your name ? "
     liftIO (putStrLn ("You, " ++ name ++ ", are " ++ age))
     t1 <- liftIO getTime
     liftIO (putStrLn ("Total time : " ++ show (t1 - t0) ++ " seconds"))
---}
---    return age
+    return (read age)
 
 getTime :: IO Integer
 getTime = do
